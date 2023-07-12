935bf32a10712d9ecc8f731d6b500a79e8c2f0d1
crisis82 <filmerlo99@gmail.com>
2023-07-13 01:15:16 +0200

    made command's choiches more clear

--- start_stop.sh
+++ start_stop.sh
@@ -15,13 +15,20 @@ ORANGE="#F09483"
 [[ $(groups | grep docker | wc -l) -eq 1 ]] && DOCKER="docker" || DOCKER="sudo docker"
 # PASS=$(gum input --password --placeholder "your user password" --header "Sudo password to run docker" --header.foreground "$WHITE")# if [[ $(groups | grep docker | wc -l) -eq 1 ]]
 
+CHOICE=$(gum choose --cursor.foreground "$ORANGE" --item.faint "Up (Directly)" "Up (Build)" "Down" "Exit" --header "What do you want to do with the Farmer?" --header.foreground "$WHITE")
 
-CHOICE=$(gum choose --cursor.foreground "$ORANGE" --item.faint "Build" "Start" "Stop" "Exit" --header "What do you want to do with the Farmer?" --header.foreground "$WHITE")
-
-if [[ $CHOICE == "Build" ]]
+if [[ $CHOICE == "Exit" ]]
 then
-  gum spin -s line --title "Taking container down ..." -- $DOCKER compose down
-  gum spin -s line --title "Rebuilding container ..." -- $DOCKER compose up -d --build
+  echo "Bye  :snake:" | gum format -t emoji | gum style --align center --padding "1 6"
+  exit 1
+fi
+
+# LOGS=$(gum choose --cursor.foreground "$ORANGE" --item.faint "Yes" " No" --header "Show logs?" --header.foreground "$WHITE")
+# [[ $LOGS == "Yes" ]] && LOGS="--show-output" || LOGS=""
+
+if [[ $CHOICE == "Up (Build)" ]]
+then
+  gum spin -s line --title "Building container ..." -- $DOCKER compose up -d --build
   if [[ $($DOCKER container ls | grep server-app | wc -l) -eq 1 ]]
   then
     echo "Container correctly $(gum style --foreground "$GREEN" --bold "UP")"
@@ -29,7 +36,7 @@ then
     echo "$(gum style --foreground "$RED" --bold "Something went wrong") ... Aborting"
     exit 1
   fi
-elif [[ $CHOICE == "Start" ]]
+elif [[ $CHOICE == "Up (Directly)" ]]
 then
   if [[ $($DOCKER container ls | grep server-app | wc -l) -eq 1 ]]
   then
@@ -49,7 +56,7 @@ then
     echo "$(gum style --foreground "$RED" --bold "Something went wrong") ... Aborting"
     exit 1
   fi
-elif [[ $CHOICE == "Stop" ]]
+elif [[ $CHOICE == "Down" ]]
 then
   if [[ $($DOCKER container ls | grep server-app | wc -l) -eq 1 ]]
   then
